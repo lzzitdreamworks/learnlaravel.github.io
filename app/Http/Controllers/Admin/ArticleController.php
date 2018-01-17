10fcf6db8cc2ad92d1c27de04f106c5c192cbac7
lzzitdreamworks <itdreamworks@163.com>
2018-01-17 22:01:03 +0800

    删除文章

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -39,4 +39,10 @@ class ArticleController extends Controller
         }
     }
 
+    public function destroy($id)
+    {
+        Article::find($id)->delete();
+        return redirect()->back()->withInput()->withErrors('删除成功！');
+    }
+
 }
